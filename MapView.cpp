54a333cd9126aa24b9aba5ad0d81a95c3f85de3f
ArtdragonXoX <1262922587@qq.com>
2024-09-17 22:29:02 +0800

    #

--- MapView.cpp
+++ MapView.cpp
@@ -70,6 +70,8 @@ void MapView::AddRoad(uint16_t id)
 	auto roadItem = new RoadRectItem(id);
 	roadItemMap.insert(id, roadItem);
 	scene->addItem(roadItem);
+	GlobalVariable::wayPointMap.value(GlobalVariable::roadMap.value(id).u).roadIds.append(id);
+	GlobalVariable::wayPointMap.value(GlobalVariable::roadMap.value(id).v).roadIds.append(id);
 }
 
 void MapView::DeleteWayPoint(uint16_t id)
